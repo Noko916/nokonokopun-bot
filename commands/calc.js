92842d9e56a2d7b303e9741a3af372165bb6a9b3
Noko <Noko916@users.noreply.github.com>
2020-08-30 06:28:28 +0900

    Add math.js & New command [.calc]
    計算させる処理を追加しました。

---
+++ commands/calc.js
@@ -0,0 +1,25 @@
+const math = require('mathjs');
+const Discord = require('discord.js');
+
+exports.run = (client, message, args, tools) => {
+
+    if (!args[0]) return message.channel.send("計算式を入力してください")
+
+    let resp;
+    
+    //処理
+    try{
+        resp = math.evalDependencies(args.join(' '));
+    } catch (e) {
+        return message.channel.send("有効な計算式を入力してください");
+    }
+
+    //Finally
+    const embed = new Discord.MessageEmbed()
+    .setColor(0xffffff)
+    .setTitle("Calc")
+    .addField("式", `\`\`\`js\n${args.join(' ')}\`\`\``)
+    .addField("計算結果", `\`\`\`js\n${resp}\`\`\``)
+
+    message.channel.send(embed);
+}
