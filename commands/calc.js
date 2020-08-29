33311d314a218481b59d18c4396b2cdd2a1e3426
Noko <Noko916@users.noreply.github.com>
2020-08-30 07:04:57 +0900

    uninstall math.js / install mathjs

--- commands/calc.js
+++ commands/calc.js
@@ -1,24 +1,26 @@
+const math = require('math.js');
 const Discord = require('discord.js');
-const math = require('mathjs');
 
-exports.run = (client, message, args, tools) => {
+exports.run = (_client, message, args, _tools) => {
 
-  if(!args[0]) return message.channel.send('Please input a calculation.')
+    if (!args[0]) return message.channel.send("計算式を入力してください")
 
-  let resp;
-  try {
-    resp = math.evaluate(args.join(' '))
-  } catch (e) {
-    return message.channel.send('Please enter a valid calculation.')
-  }
+    let resp;
+    
+    //処理
+    try{
+        resp = math.evaluate(args.join(' '));
+    } catch (e) {
+        console.log(e);
+        return message.channel.send("有効な計算式を入力してください");
+    }
 
+    //Finally
+    const embed = new Discord.MessageEmbed()
+    .setColor(0xffffff)
+    .setTitle("Calc")
+    .addField("式", `\`\`\`js\n${args.join(' ')}\`\`\``)
+    .addField("計算結果", `\`\`\`js\n${resp}\`\`\``)
 
-  const embed = new Discord.RichEmbed()
-    .setColor(0x808080)
-    .setTitle('Math Calculation')
-    .addField('Input', `\`\`\`css\n${args.join('')}\`\`\``)
-    .addField('Output', `\`\`\`css\n${resp}\`\`\``)
-
-message.channel.send(embed);
-
+    message.channel.send(embed);
 }
