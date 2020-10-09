c212a77d496fade10f6802fd5db1e779b418930a
Noko <Noko916@users.noreply.github.com>
2020-10-09 18:09:56 +0900

    a

--- commands/s001.js
+++ commands/s001.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   var cont = message.content;
   var rep = cont.replace(".repeat ", "");
-  var ID = 257081353663676421;
+  var ID = 664453697425768527;
 
   client.channels.cache.get(ID).send(rep)
   return;
