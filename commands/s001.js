cd5fe7e2f65fdf708d49f7963480bda0de3fb35c
Noko <Noko916@users.noreply.github.com>
2020-10-09 18:51:24 +0900

    Update s001.js

--- commands/s001.js
+++ commands/s001.js
@@ -3,7 +3,11 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   var cont = message.content;
   var rep = cont.replace(".s001 ", "");
-  var ID = `664453697425768527`;
+  
+  
+  var ID = `257081353663676421`;
+  /* テストサーバー #一般*/
+  //var ID = `664453697425768527`;
 
   client.channels.cache.get(ID).send(rep);
   return;
