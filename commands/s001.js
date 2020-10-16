683207d0773dfabbdee7375e65751264c155ad3b
Noko <Noko916@users.noreply.github.com>
2020-10-17 02:44:10 +0900

    Update s001.js

--- commands/s001.js
+++ commands/s001.js
@@ -4,7 +4,6 @@ exports.run = (client, message, args) => {
   var cont = message.content;
   var rep = cont.replace(".s001 ", "");
   
-  
   var ID = `257081353663676421`;
   /* テストサーバー #一般*/
   //var ID = `664453697425768527`;
