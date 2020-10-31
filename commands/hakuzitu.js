fbed201288bc62b838a751c417dfaae7777850ed
Noko <Noko916@users.noreply.github.com>
2020-11-01 03:31:07 +0900

    1

--- commands/hakuzitu.js
+++ commands/hakuzitu.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
   
-    message.channel.send("https://www.youtube.com/watch?v=ony539T074w");
+    message.channel.send("正解!\nhttps://www.youtube.com/watch?v=ony539T074w");
     return;
   
 };
