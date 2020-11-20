6d834ea2e7723f4e074a02af407126d48fd8c70c
Noko <Noko916@users.noreply.github.com>
2020-11-20 18:16:08 +0900

    Create 3renkyu.js

---
+++ commands/3renkyu.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+  
+  message.channel.send(
+    "https://docs.google.com/spreadsheets/d/1tSukplXqESD0D-unRycyAdSdYYY1SknbZRhQ-OLg-H0/edit?usp=sharing"
+  );
+  return;
+  
+};
