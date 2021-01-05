0a19320e52b6b03e8c9b483048d99fd11cf68743
Noko <Noko916@users.noreply.github.com>
2021-01-05 21:51:55 +0900

    Create easing.js
    自分用

---
+++ commands/easing.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+    message.channel.send("https://aketama.work/wp-content/uploads/2018/09/easing-1.png");
+
+    return;
+}
