18a2e30f49a6560254d17014db7e9faca5257f15
Noko <Noko916@users.noreply.github.com>
2021-02-22 21:59:21 +0900

    Update bank.js

--- commands/bank.js
+++ commands/bank.js
@@ -9,7 +9,7 @@ const EmbedBank = new Discord.MessageEmbed()
 
 const EmbedBank2 = new Discord.MessageEmbed()
   .addField("にこにこぷんBank :","[[ **Here** ](https://docs.google.com/spreadsheets/d/1w0ZvpBs0P0e-l-0emYZsQYfRWbBKBtbwsWYAPEd6VyE/edit#gid=977767115)]")
-  .addField("サバイバルシューター :","[[ **ルール** ](https://docs.google.com/document/d/1AYtP4O6dTp1NJsKqBVGuNmzHWTqveZzi_66Pl2TnQRY/edit)]　[[ **関連シート** ](https://docs.google.com/spreadsheets/d/1DsgZ3rb2b3XhdAjx7z9lavOoZZ3EFAQmKgXZG3VMGSU/edit#gid=364382638)]")
+  .addField("シーズン企画「アトリエ」 :","[[ **ルール** ](https://docs.google.com/document/d/1lXMIvFi7IxdYBtovydpRQteRk36LiJ9Bpjpva1am4yk/edit)]　[[ **企画シート** ](https://docs.google.com/spreadsheets/d/1s2QfczDzd0bpGneXdl3wmcfbyfjDS4GbjTBKshSnwF8/edit#gid=0)]")
   .setColor(1752220);
 
 
