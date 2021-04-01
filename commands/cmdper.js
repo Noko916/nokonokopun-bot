1c3ce9d56248f06a4b458f46bc8315c389ca5ea7
Noko <nokohrest@gmail.com>
2021-04-01 13:00:18 +0900

    Update v102
    msg -> message
    50%以上の場合に警告を表示

--- commands/cmdper.js
+++ commands/cmdper.js
@@ -3,21 +3,32 @@ const db        = require("quick.db");
 
 module.exports = {
     name: "cmdper",
-    description: "コマンド拒否率を変えるか参照します",
+    description: `コマンド拒否率を変更,表示します\n\`.cmdper <h/o> <set/get> <percent> (OK)\``,
     aliases: ["percent", "per"],
 
     async execute(client, message, args) {
 
-        if(args[0] == "set") {
-            
-            if(args[1] == "h") db.set('h_per', args[2]);
-            if(args[2] == "o") db.set('o_per', args[2]);
+        if (args[0] == "set") {
+
+            if (args[2] >= 50) {
+
+                if (args[3] != "OK") {
+                    `値が50以上です\nそれでもいい場合は最後に \`OK\` を足して再度コマンドを打ち直してください`
+                    return;
+                }
+
+            }
+
+            if (args[1] == "h") db.set('h_per', args[2]);
+            if (args[1] == "o") db.set('o_per', args[2]);
+
+            message.channel.send(`${args[1]}_per を \`${args[2]}%\` に設定しました`)
         }
 
-        if(args[0] == "get") {
+        if (args[0] == "get") {
 
-            if(args[1] == "h") msg.channel.send(db.get('h_per'));
-            if(args[1] == "o") msg.channel.send(db.get('o_per'));
+            if (args[1] == "h") message.channel.send(`h_per は \`${db.get('h_per')}%\` です`);
+            if (args[1] == "o") message.channel.send(`o_per は \`${db.get('o_per')}%\` です`);
         }
 
         return;
