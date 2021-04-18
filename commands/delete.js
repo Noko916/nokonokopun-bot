62315624ed36364e1616e22bf6cea38bc4f02a29
Noko <nokohrest@gmail.com>
2021-04-19 06:41:22 +0900

    Update v108
    delete.js
     helpの説明を変更
     消去上限の設定
     消去したメッセージをログに出力するよう修正

--- commands/delete.js
+++ commands/delete.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js");
+const LimitNum = 20;
 
 module.exports = {
     name: "delete",
-    description: "メッセージを消去します 複数同時に消去可能\n\`.delete [件数]\`",
+    description: `メッセージを消去します\n${LimitNum} 件まで一括で消去可能\n14 日以上前のメッセージが含まれている時は削除できません\n\`.delete [件数]\``,
 
     async execute(client, message, args) {
 
         if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Error: 権限がありません");
+
+        if (args[0] > LimitNum) return message.channel.send(`${LimitNum} 件までにしてください`);
 
         let limitMsg = 0;
 
@@ -15,9 +18,15 @@ module.exports = {
         limitMsg = Number(limitMsg) + 1;
 
         const dMsg = await message.channel.messages.fetch({ limit: limitMsg });
-        message.channel.bulkDelete(dMsg);
+        
+        dMsg.forEach(msg => {
+            console.log(` >> [${msg.createdAt}] ${msg.author.tag}: ${msg.content}`);
+        });
+        
+        console.log();
 
-        message.channel.send(`${message.member.displayName} が ${args[0]} 件のメッセージを消去しました`);
+        message.channel.bulkDelete(dMsg);
+        message.channel.send(`${message.member.displayName} が ${limitMsg - 1} 件のメッセージを消去しました`);
 
         //message.channel.send(`\`\`\`args  ` + args + `\nlimit ` + limitMsg + `\`\`\``);
 
