b3506397a97374dd6a7faa5e8fd578f84cfdde29
Noko <nokohrest@gmail.com>
2021-07-19 02:00:37 +0900

    Update hakujitsu.js

--- commands/hakujitsu.js
+++ commands/hakujitsu.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports = {
-    name: "白日",
+    name: "hakuzitu",
     description: "白日を貼ります 不定期でコマンドが変わります",
   
     async execute(client, message, args) {
